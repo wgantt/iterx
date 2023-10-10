e39653d8e711939b3d9222557ad7e9868ec6a3fe
Will Gantt <wgantt.iv@gmail.com>
2023-10-10 16:59:45 -0400

    fix predict_all.sh script

--- scripts/muc/predict_all.sh
+++ scripts/muc/predict_all.sh
@@ -6,13 +6,11 @@ LANGS=(ar fa ko ru zh)
 CHECKPOINTS_DIR=/brtx/604-nvme2/wgantt/better/checkpoints/multimuc/iterx/
 
 
-# for lang in "${LANGS[@]}"; do
-#   for setting in bi; do
-for lang in fa; do
-	for setting in bi; do
-	sbatch scripts/muc/predict_iterx.sh \
-	  $CHECKPOINTS_DIR/$setting/$lang \
-	  $lang \
-	  $setting
+for lang in ${LANGS[@]}; do
+	for setting in ${SETTINGS[@]}; do
+		sbatch scripts/muc/predict_iterx.sh \
+		$CHECKPOINTS_DIR/$setting/$lang-mt5-large \
+		$lang \
+		$setting
   done
 done
